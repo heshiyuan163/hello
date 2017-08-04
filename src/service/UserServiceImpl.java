1d3a062e01ce9a8290bd4cdafe5e1e88159ea418
heshiyuanqq <1406146854@qq.com>
2017-08-04 17:27:19 +0800

    asfdasfsdfsd

---
+++ src/service/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package service;
+
+import java.util.List;
+
+import pojo.User;
+
+public class UserServiceImpl implements UserService{
+
+	@Override
+	public User getUserByUsername(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> getUserListByAge(Integer age) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
